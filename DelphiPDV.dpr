6127abfb59598780c664ef142fbec54c5afd94ca
albertoparente <albertoparentefh@gmail.com>
2023-05-10 23:17:57 -0300

    Adding +

--- DelphiPDV.dpr
+++ DelphiPDV.dpr
@@ -2,7 +2,8 @@ program DelphiPDV;
 
 uses
   Vcl.Forms,
-  DelphiPDV.View.Principal in 'src\view\DelphiPDV.View.Principal.pas' {FormPrincipal};
+  DelphiPDV.View.Principal in 'src\view\DelphiPDV.View.Principal.pas' {FormPrincipal},
+  DelphiPDV.View.Produto in 'src\View\DelphiPDV.View.Produto.pas' {Form1};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFormPrincipal, FormPrincipal);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
