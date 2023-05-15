3070827667f1ad570589e6c1e61a6b9e517928af
albertoparente <albertoparentefh@gmail.com>
2023-05-14 23:46:43 -0300

    Adding +++

--- DelphiPDV.dpr
+++ DelphiPDV.dpr
@@ -3,7 +3,8 @@ program DelphiPDV;
 uses
   Vcl.Forms,
   DelphiPDV.View.Principal in 'src\view\DelphiPDV.View.Principal.pas' {FormPrincipal},
-  DelphiPDV.View.Produto in 'src\View\DelphiPDV.View.Produto.pas' {Produto};
+  DelphiPDV.View.Produto in 'src\View\DelphiPDV.View.Produto.pas' {Produto},
+  DelphiPDV.View.Vendedor in 'src\View\DelphiPDV.View.Vendedor.pas' {Vendedor};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFormPrincipal, FormPrincipal);
   Application.CreateForm(TProduto, Produto);
+  Application.CreateForm(TVendedor, Vendedor);
   Application.Run;
 end.
--- src/View/DelphiPDV.View.Vendedor.pas
+++ src/View/DelphiPDV.View.Vendedor.pas
@@ -4,68 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, cxGraphics, cxControls, cxLookAndFeels,
-  cxLookAndFeelPainters, cxContainer, cxEdit, dxSkinsCore, dxSkinBasic,
-  dxSkinBlack, dxSkinBlue, dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee,
-  dxSkinDarkroom, dxSkinDarkSide, dxSkinDevExpressDarkStyle,
-  dxSkinDevExpressStyle, dxSkinFoggy, dxSkinGlassOceans, dxSkinHighContrast,
-  dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
-  dxSkinMcSkin, dxSkinMetropolis, dxSkinMetropolisDark, dxSkinMoneyTwins,
-  dxSkinOffice2007Black, dxSkinOffice2007Blue, dxSkinOffice2007Green,
-  dxSkinOffice2007Pink, dxSkinOffice2007Silver, dxSkinOffice2010Black,
-  dxSkinOffice2010Blue, dxSkinOffice2010Silver, dxSkinOffice2013DarkGray,
-  dxSkinOffice2013LightGray, dxSkinOffice2013White, dxSkinOffice2016Colorful,
-  dxSkinOffice2016Dark, dxSkinOffice2019Black, dxSkinOffice2019Colorful,
-  dxSkinOffice2019DarkGray, dxSkinOffice2019White, dxSkinPumpkin, dxSkinSeven,
-  dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus, dxSkinSilver,
-  dxSkinSpringtime, dxSkinStardust, dxSkinSummer2008, dxSkinTheAsphaltWorld,
-  dxSkinTheBezier, dxSkinsDefaultPainters, dxSkinValentine,
-  dxSkinVisualStudio2013Blue, dxSkinVisualStudio2013Dark,
-  dxSkinVisualStudio2013Light, dxSkinVS2010, dxSkinWhiteprint,
-  dxSkinXmas2008Blue, cxStyles, cxCustomData, cxFilter, cxData, cxDataStorage,
-  cxNavigator, dxDateRanges, dxScrollbarAnnotations, Data.DB, cxDBData,
-  cxTextEdit, cxCurrencyEdit, cxGridLevel, cxGridCustomLayoutView,
-  cxGridCardView, cxGridDBCardView, cxGridCustomTableView, cxGridTableView,
-  cxGridDBTableView, cxClasses, cxGridCustomView, cxGrid, Vcl.StdCtrls,
-  Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
 
 type
   TVendedor = class(TForm)
-    Panel1: TPanel;
-    Shape1: TShape;
-    Panel3: TPanel;
-    Label6: TLabel;
-    EditPesquisaProduto: TcxTextEdit;
-    DBGrid: TcxGrid;
-    GridView: TcxGridDBTableView;
-    ColunaPbm: TcxGridDBColumn;
-    ColunaTipo: TcxGridDBColumn;
-    ColunaCodigo: TcxGridDBColumn;
-    ColunaCodBarras: TcxGridDBColumn;
-    ColunaDescricao: TcxGridDBColumn;
-    ColunaUnidade: TcxGridDBColumn;
-    ColunaFabricante: TcxGridDBColumn;
-    ColunaSaldo: TcxGridDBColumn;
-    ColunaPrecoAnterior: TcxGridDBColumn;
-    ColunaPercentualDesconto: TcxGridDBColumn;
-    ColunaPreco: TcxGridDBColumn;
-    ColunaRentabilidade: TcxGridDBColumn;
-    ColunaCtrVenda: TcxGridDBColumn;
-    ColunaDesPromoc: TcxGridDBColumn;
-    ColunaPrcBonVdr: TcxGridDBColumn;
-    ColunaFlgBlqDsc: TcxGridDBColumn;
-    ColunaChavePbm: TcxGridDBColumn;
-    ColunaPortaria: TcxGridDBColumn;
-    ColunaPossuiDescontos: TcxGridDBColumn;
-    DBGridDBCardView1: TcxGridDBCardView;
-    DBGridLevel1: TcxGridLevel;
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
-    procedure FormShow(Sender: TObject);
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormShow(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
     { Private declarations }
   public
     { Public declarations }
+    Procedure Process;
+    Procedure Review;
   end;
 
 var
@@ -100,8 +51,17 @@ begin
   end;
 end;
 
-
 procedure TVendedor.FormShow(Sender: TObject);
+begin
+//
+end;
+
+procedure TVendedor.Process;
+begin
+//
+end;
+
+procedure TVendedor.Review;
 begin
 //
 end;
