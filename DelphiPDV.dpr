833b3a14a0005c39974ba4afd20643a9ae3d9d2f
albertoparente <albertoparentefh@gmail.com>
2023-10-01 18:26:52 -0300

    Adjusting +

--- DelphiPDV.dpr
+++ DelphiPDV.dpr
@@ -30,7 +30,8 @@ uses
   DelphiPDV.View.Other1 in 'src\View\DelphiPDV.View.Other1.pas' {VwOther1},
   DelphiPDV.View.Other2 in 'src\View\DelphiPDV.View.Other2.pas' {VwOther2},
   DelphiPDV.View.Other3 in 'src\View\DelphiPDV.View.Other3.pas' {VwOther3},
-  DelphiPDV.View.Other4 in 'src\View\DelphiPDV.View.Other4.pas' {VwOther4};
+  DelphiPDV.View.Other4 in 'src\View\DelphiPDV.View.Other4.pas' {VwOther4},
+  DelphiPDV.View.Other5 in 'src\View\DelphiPDV.View.Other5.pas' {Form1};
 
 {$R *.res}
 
@@ -41,5 +42,6 @@ begin
   Application.CreateForm(TdmDados, dmDados);
   Application.CreateForm(TVwPrincipal, VwPrincipal);
   Application.CreateForm(TVwOther4, VwOther4);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
