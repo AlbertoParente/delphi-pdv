69a1ba71676ea74d2058aa634bcb3d12a8322a14
albertoparente <albertoparentefh@gmail.com>
2024-01-02 23:53:33 -0300

    Adding ++

--- src/View/DelphiPDV.View.Other49.pas
+++ src/View/DelphiPDV.View.Other49.pas
@@ -29,7 +29,7 @@ uses
   cxClasses, cxGridCustomView, cxGrid;
 
 type
-  TForm4 = class(TForm)
+  TVwOther49 = class(TForm)
     DBGrid: TcxGrid;
     GridView: TcxGridDBTableView;
     ColumnCodigo: TcxGridDBColumn;
@@ -58,20 +58,32 @@ type
     aDataSource: TDataSource;
     dsItens: TDataSource;
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure GridViewCustomDrawCell(Sender: TcxCustomGridTableView;
+      ACanvas: TcxCanvas; AViewInfo: TcxGridTableDataCellViewInfo;
+      var ADone: Boolean);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
+    procedure Process;
+    procedure Review;
   end;
 
 var
-  Form4: TForm4;
+  VwOther49: TVwOther49;
 
 implementation
 
 {$R *.dfm}
 
-procedure TForm4.FormKeyDown(Sender: TObject; var Key: Word;
+procedure TVwOther49.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  Action := caFree;
+end;
+
+procedure TVwOther49.FormKeyDown(Sender: TObject; var Key: Word;
   Shift: TShiftState);
 begin
   inherited;
@@ -97,6 +109,35 @@ begin
       aDataSource.DataSet.Next;
     end;
   end;
+end;
+
+procedure TVwOther49.FormShow(Sender: TObject);
+begin
+  EditPesquisa.Clear;
+  aDataSource.DataSet := nil;
+end;
+
+procedure TVwOther49.GridViewCustomDrawCell(Sender: TcxCustomGridTableView;
+  ACanvas: TcxCanvas; AViewInfo: TcxGridTableDataCellViewInfo;
+  var ADone: Boolean);
+begin
+  inherited;
+  if AViewInfo.GridRecord.Selected then
+  begin
+    ACanvas.Brush.Color := clHighlight;
+    ACanvas.Font.Color  := clWhite;
+  end;
+end;
+
+procedure TVwOther49.Process;
+begin
+  Review;
+  Close;
+end;
+
+procedure TVwOther49.Review;
+begin
+
 end;
 
 end.
