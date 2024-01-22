31210a6d64314c0976ed5e159ef25a97be2d6296
albertoparente <albertoparentefh@gmail.com>
2024-01-22 20:28:48 -0300

    Adding ++

---
+++ src/View/DelphiPDV.View.Other8.pas
@@ -0,0 +1,73 @@
+unit DelphiPDV.View.Other8;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, cxGraphics, cxControls, cxLookAndFeels,
+  cxLookAndFeelPainters, dxSkinsCore, dxSkinBasic, dxSkinBlack, dxSkinBlue,
+  dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee, dxSkinDarkroom, dxSkinDarkSide,
+  dxSkinDevExpressDarkStyle, dxSkinDevExpressStyle, dxSkinFoggy,
+  dxSkinGlassOceans, dxSkinHighContrast, dxSkiniMaginary, dxSkinLilian,
+  dxSkinLiquidSky, dxSkinLondonLiquidSky, dxSkinMcSkin, dxSkinMetropolis,
+  dxSkinMetropolisDark, dxSkinMoneyTwins, dxSkinOffice2007Black,
+  dxSkinOffice2007Blue, dxSkinOffice2007Green, dxSkinOffice2007Pink,
+  dxSkinOffice2007Silver, dxSkinOffice2010Black, dxSkinOffice2010Blue,
+  dxSkinOffice2010Silver, dxSkinOffice2013DarkGray, dxSkinOffice2013LightGray,
+  dxSkinOffice2013White, dxSkinOffice2016Colorful, dxSkinOffice2016Dark,
+  dxSkinOffice2019Black, dxSkinOffice2019Colorful, dxSkinOffice2019DarkGray,
+  dxSkinOffice2019White, dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic,
+  dxSkinSharp, dxSkinSharpPlus, dxSkinSilver, dxSkinSpringtime, dxSkinStardust,
+  dxSkinSummer2008, dxSkinTheAsphaltWorld, dxSkinTheBezier,
+  dxSkinsDefaultPainters, dxSkinValentine, dxSkinVisualStudio2013Blue,
+  dxSkinVisualStudio2013Dark, dxSkinVisualStudio2013Light, dxSkinVS2010,
+  dxSkinWhiteprint, dxSkinXmas2008Blue, cxStyles, cxCustomData, cxFilter,
+  cxData, cxDataStorage, cxEdit, cxNavigator, dxDateRanges,
+  dxScrollbarAnnotations, Data.DB, cxDBData, cxTextEdit, Vcl.StdCtrls,
+  Vcl.ExtCtrls, cxGridLevel, cxGridCustomLayoutView, cxGridCardView,
+  cxGridDBCardView, cxGridCustomTableView, cxGridTableView, cxGridDBTableView,
+  cxClasses, cxGridCustomView, cxGrid;
+
+type
+  TForm6 = class(TForm)
+    DBGrid: TcxGrid;
+    GridView: TcxGridDBTableView;
+    ColumnCodigo: TcxGridDBColumn;
+    ColumnSellerName: TcxGridDBColumn;
+    DBGridDBCardView1: TcxGridDBCardView;
+    DBGridLevel1: TcxGridLevel;
+    PanelPesquisa: TPanel;
+    LabelPesquisa: TLabel;
+    EditPesquisa: TEdit;
+    PanelInformation: TPanel;
+    LabelInformation: TLabel;
+    aDataSource: TDataSource;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    procedure Process;
+    procedure Review;
+  end;
+
+var
+  Form6: TForm6;
+
+implementation
+
+{$R *.dfm}
+
+{ TForm6 }
+
+procedure TForm6.Process;
+begin
+  Review;
+  Close;
+end;
+
+procedure TForm6.Review;
+begin
+
+end;
+
+end.
