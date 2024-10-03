3bffa063e31c8c163e4d82fa9b8d8a59710d9eec
albertoparente <albertoparentefh@gmail.com>
2024-10-02 21:40:41 -0300

    Adding +++

--- DelphiPDV.dpr
+++ DelphiPDV.dpr
@@ -77,7 +77,8 @@ uses
   DelphiPDV.View.Other247 in 'src\View\DelphiPDV.View.Other247.pas' {Form246},
   DelphiPDV.View.Other248 in 'src\View\DelphiPDV.View.Other248.pas' {Form8},
   DelphiPDV.View.Other10 in 'src\View\DelphiPDV.View.Other10.pas' {Form9},
-  DelphiPDV.View.Other11 in 'src\View\DelphiPDV.View.Other11.pas' {Form10};
+  DelphiPDV.View.Other11 in 'src\View\DelphiPDV.View.Other11.pas' {Form10},
+  DelphiPDV.View.Other12 in 'src\View\DelphiPDV.View.Other12.pas' {Form11};
 
 {$R *.res}
 
@@ -101,5 +102,6 @@ begin
   Application.CreateForm(TForm8, Form8);
   Application.CreateForm(TForm9, Form9);
   Application.CreateForm(TForm10, Form10);
+  Application.CreateForm(TForm11, Form11);
   Application.Run;
 end.
